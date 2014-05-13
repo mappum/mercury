b9f646a6260e065ba1b8310927b24b257bed6765
Matt Bell <mappum@gmail.com>
2014-05-12 20:35:53 -0700

    Updated placeholder markup

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -1,3 +1,41 @@
+(function(window) {
+  var handlers = {};
+
+  window.controller = {
+    emit: function(id, event, data) {
+      if(data == null) {
+        data = event;
+        event = id;
+        id = '';
+      }
+
+      if(!handlers[id]) return;
+      if(!handlers[id][event]) return;
+
+      for(var cb in handlers[id][event]) {
+        cb(data);
+      }
+    },
+
+    on: function(id, event, cb) {
+      if(cb == null) {
+        cb = event;
+        event = id;
+        id = null;
+      }
+
+      if(!handlers[id]) handlers[id] = {};
+      
+      var h = handlers[id][event];
+      if(!h) handlers[id][event] = [];
+
+      handlers[id][event].push(cb);
+    }
+  };
+})(window);
+
 $(function(){
   $('.btn').button();
 });
+
+document.write(navigator);
