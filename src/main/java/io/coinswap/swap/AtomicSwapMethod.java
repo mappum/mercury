7f6749c8be4f05059d0d0149633b3001ef68bc4e
Matt Bell <mappum@gmail.com>
2014-06-10 18:18:05 -0700

    Added atomic swap network method names

---
+++ src/main/java/io/coinswap/swap/AtomicSwapMethod.java
@@ -0,0 +1,12 @@
+package io.coinswap.swap;
+
+public class AtomicSwapMethod {
+    public static String
+        VERSION = "version",
+        KEYS_REQUEST = "keys.request",
+        BAILIN_HASH_REQUEST = "keys.request",
+        EXCHANGE_SIGNATURES = "exchange.signatures",
+        EXCHANGE_BAIL_IN = "exchange.bail.in",
+        CANCEL_TRANSACTION = "cancel.transaction",
+        ARBITRATE = "arbitrate";
+}
