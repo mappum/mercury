cc4b3853f75e8f2a6c2434a6d98cccea67f3c1d0
Matt Bell <mappum@gmail.com>
2014-12-13 03:00:17 -0800

    Bitcoin and Litecoin now support nonstandard TXs

--- src/main/java/io/coinswap/Coins.java
+++ src/main/java/io/coinswap/Coins.java
@@ -13,16 +13,20 @@ public class Coins {
 
         currencies.add(new Currency(MainNetParams.get(), dataDir,
                 "Bitcoin", "BTC", "<i class=\"fa fa-bitcoin\"></i>",
-                new String[]{"LTC", "DOGE", "BTCt", "LTCt"}, currencies.size(), false));
+                new String[]{"LTC", "DOGE", "BTCt", "LTCt"}, currencies.size(), true));
+
         currencies.add(new Currency(LitecoinMainNetParams.get(), dataDir,
                 "Litecoin", "LTC", "&#321;",
-                new String[]{"BTC", "DOGE", "LTCt"}, currencies.size(), false));
+                new String[]{"BTC", "DOGE", "LTCt"}, currencies.size(), true));
+
         currencies.add(new Currency(DogecoinMainNetParams.get(), dataDir,
                 "Dogecoin", "DOGE", "&#272;",
                 new String[] { "BTC", "LTC" }, currencies.size(), false));
+
         currencies.add(new Currency(TestNet3Params.get(), dataDir,
                 "Bitcoin Testnet", "BTCt", "<i class=\"fa fa-bitcoin\"></i>",
                 new String[]{"BTC", "LTCt"}, currencies.size(), true));
+
         currencies.add(new Currency(LitecoinTestNetParams.get(), dataDir,
                 "Litecoin Testnet", "LTCt", "&#321;",
                 new String[] { "BTC", "LTC", "BTCt" }, currencies.size(), true));
