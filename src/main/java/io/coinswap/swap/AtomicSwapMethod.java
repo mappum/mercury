b945d8afc7f950e5745372486f5849ff6d853580
Matt Bell <mappum@gmail.com>
2014-12-19 19:04:42 -0800

    AtomicSwapMethod: Fixed method string for BAILIN_HASH_REQUEST

--- src/main/java/io/coinswap/swap/AtomicSwapMethod.java
+++ src/main/java/io/coinswap/swap/AtomicSwapMethod.java
@@ -4,7 +4,7 @@ public class AtomicSwapMethod {
     public static String
         VERSION = "version",
         KEYS_REQUEST = "keys.request",
-        BAILIN_HASH_REQUEST = "keys.request",
+        BAILIN_HASH_REQUEST = "bail_in_hash.request",
         EXCHANGE_SIGNATURES = "exchange.signatures",
         EXCHANGE_BAIL_IN = "exchange.bail.in",
         CANCEL_TRANSACTION = "cancel.transaction",
