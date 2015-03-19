4ea561341f5ded9192a35cb344e2417b309b336a
Matt Bell <mappum@gmail.com>
2015-03-18 19:55:30 -0700

    Removed Bitcoin testnet from active coins

--- src/main/java/io/coinswap/Coins.java
+++ src/main/java/io/coinswap/Coins.java
@@ -13,19 +13,19 @@ public class Coins {
 
         currencies.add(new Currency(MainNetParams.get(), dataDir,
                 "Bitcoin", "BTC", "<i class=\"fa fa-bitcoin\"></i>",
-                new String[]{"LTC","DOGE","BTCt"}, currencies.size(), true, 1));
+                new String[]{"LTC","DOGE"}, currencies.size(), true, 1));
 
         currencies.add(new Currency(LitecoinMainNetParams.get(), dataDir,
                 "Litecoin", "LTC", "&#321;",
-                new String[]{"BTC","BTCt"}, currencies.size(), false, 5));
+                new String[]{"BTC"}, currencies.size(), false, 5));
 
         currencies.add(new Currency(DogecoinMainNetParams.get(), dataDir,
                 "Dogecoin", "DOGE", "&#272;",
-                new String[] {"BTC","BTCt"}, currencies.size(), false, 5));
+                new String[] {"BTC"}, currencies.size(), false, 5));
 
-        currencies.add(new Currency(TestNet3Params.get(), dataDir,
+        /*currencies.add(new Currency(TestNet3Params.get(), dataDir,
                 "Bitcoin Testnet", "BTCt", "<i class=\"fa fa-bitcoin\"></i>",
-                new String[]{"BTC","LTC","DOGE"}, currencies.size(), true, 1));
+                new String[]{"BTC","LTC","DOGE"}, currencies.size(), true, 1));*/
 
         /*currencies.add(new Currency(LitecoinTestNetParams.get(), dataDir,
                 "Litecoin Testnet", "LTCt", "&#321;",
